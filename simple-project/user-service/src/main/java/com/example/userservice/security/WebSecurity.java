8b5324d223ee8107271886563332d400de2928ff
이석준 <leesukjune@wemakeprice.com>
2021-08-07 15:20:18 +0900

    user-service 에 spring-cloud-config 값 읽을 수 있도록 추가

--- simple-project/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ simple-project/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -25,8 +25,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("192.168.219.100")
+                .hasIpAddress("192.168.40.48")
                 .and()
                 .addFilter(getAuthenticationFilter());
 
