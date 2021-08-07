8abfaa0363931369d7f902fd5db713e8afa76efd
이석준 <leesukjune@wemakeprice.com>
2021-08-07 15:51:47 +0900

    gateway service 에 spring-cloud 읽을 수 있도록 추가

--- simple-project/gateway-service/src/main/java/com/example/servicegateway/ServiceGatewayApplication.java
+++ simple-project/gateway-service/src/main/java/com/example/servicegateway/ServiceGatewayApplication.java
@@ -1,11 +1,19 @@
 package com.example.servicegateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServiceGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 }
